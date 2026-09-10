04846403a1317b1e826f9d0b17ecaac916276f08
Grace <20106694@mail.wit.ie>
2026-09-10 13:39:05 +0100

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -3,6 +3,7 @@ import { colleagues, friends } from './01-basics';
 
 function older(f: Friend) : string {
      f.age += 1
+
      return `${f.name} is now ${f.age}` 
 }
 
@@ -39,8 +40,13 @@ function addColleague(
     return cs;
 }
 
-addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
-console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
+addColleague(
+    colleagues.current, "Sheild O Connell", "HR", "soc@here.com"
+);
+
+console.log(
+    colleagues.current.filter((c) => c.name === "Sheild O Connell"
+));
 
 
 function sortColleagues(
@@ -48,7 +54,9 @@ function sortColleagues(
   sorter: (c1: Colleague, c2: Colleague) => number,
   max? : number
 ): EmailContact[] {
+
   let end = colleagues.length;
+
   if (max !== undefined) {
      end = max < 2 ? 1 : max
   }
@@ -56,6 +64,7 @@ function sortColleagues(
   const fullResult =  sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
   return fullResult.slice(0,end)
 }
+
 // Test invocations
 console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
 console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
@@ -82,3 +91,19 @@ console.log(
         (friend) => friend.age < 35
     )
 );
+
+
+function addInterest(
+    friend: Friend, 
+    interest: string
+): string[] {
+    if (friend.interests === undefined) {
+        friend.interests = [];
+    }
+    friend.interests.push(interest);
+    return friend.interests;
+}
+
+console.log(
+    addInterest(friends[0], "Politics")
+)
